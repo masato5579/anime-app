5d9e33097d9d84788c6c93b8ed43018abd352bce
masato5579 <73378197+masato5579@users.noreply.github.com>
2023-01-30 22:10:47 +0900

    [add] php-cs-fixer導入 & 整形

--- anime-server/app/Http/Controllers/SignupController.php
+++ anime-server/app/Http/Controllers/SignupController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SignUpController extends Controller
 {
@@ -23,11 +25,19 @@ class SignUpController extends Controller
     {
         $input = $request->only([
             'email',
-            'password',
-            'image',
             'name',
             'age',
             'sex',
+        ]);
+
+        $path = $request->hasFile('image')
+            ? $request->file('image')->store('profile', 'public')
+            : null;
+
+        User::create([
+            ...$input,
+            'password' => Hash::make($request->password),
+            'image' => $path,
         ]);
 
         return response()->json(['confirmed' => true]);
